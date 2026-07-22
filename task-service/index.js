16797c05001975046ea4ce46e6a1e1e418a9dad7
patrickb-max <patrick.b@futuristglobal.com>
2026-07-22 11:55:21 +0000

    added CURD operations in index.js file

--- task-service/index.js
+++ task-service/index.js
@@ -15,9 +15,36 @@ app.get("/", (req, res) => {
   res.send("Backend is alive!");
 });
 
-// Example route
+//In -memory tasks list
+let tasks = [{ id: 1, title: "Learn Svelte", completed: false }];
+
+// CURD routes
+
+// GET Method
 app.get("/tasks", (req, res) => {
-  res.json([{ id: 1, title: "Learn Svelte" }]);
+  res.json(tasks);
 });
+
+// POST Method
+app.post("/tasks", (req, res) => {
+  const newTask = { id: Date.now(), ...req.body };
+  tasks.push(newTask);
+  res.json(newTask);
+});
+
+// PUT Method
+app.put("/tasks/:id", (req, res) => {
+  const id = Number(req.params.id);
+  tasks = tasks.map(t => t.id === id ? { ...t, ...req.body } : t);
+  res.json(tasks.find(t => t.id === id));
+});
+
+// DELETE Method
+app.delete("/tasks/:id", (req, res) => {
+  const id = Number(req.params.id);
+  tasks = tasks.filter(t => t.id !== id);
+  res.json({ message: "Task deleted" });
+});
+
 
 app.listen(5000, () => console.log("Task Service running on 5000"));
